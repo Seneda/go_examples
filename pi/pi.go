a9d35523aba43a164fe2d9850bdca6da784e6b0e
Seneda <senedanassirali@gmail.com>
2017-07-09 11:45:29 +0100

    Add a Pi calculator

---
+++ pi/pi.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+import "math"
+
+
+func Pi() (p float64){
+    for i := 1.0; i < 1000000000; i = i+4 {
+        p += 4 / i
+        p -= 4 / (i + 2)
+    }
+    return
+}
+
+func main() {
+    p := Pi()
+    fmt.Printf("Pi()    : %.15f\n", p)
+    fmt.Printf("math.Pi : %.15f\n", math.Pi)
+    fmt.Printf("Difference between math.Pi and Pi() is : %e", p-math.Pi)
+}
