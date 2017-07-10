d3ca105ca6ff1af252158afedfa0b7efc5651473
Seneda <senedanassirali@gmail.com>
2017-07-10 21:48:44 +0100

    Add a basic implementation of quicksort

--- sort/quick.go
+++ sort/quick.go
@@ -1,6 +1,23 @@
 package sort
 
 func Quick(l []int) []int {
-	// TODO implement the sort
-	return l
+	if len(l) <= 1 {
+		return l
+	}
+ 	less, more := Partition(l)
+	return append(Quick(less), Quick(more)...)
+}
+
+func Partition(l []int) ([]int, []int){
+	pivot := l[len(l)-1]
+	less := []int{}
+	more := []int{pivot}
+	for _, v := range l[:len(l)-1] {
+		if v <= pivot {
+			less = append(less, v)
+		} else {
+			more = append(more, v)
+		}
+	}
+	return less, more
 }
